5c45b37a1261ccbcede69382090d30f139d4130b
Rafael Julio <development@rafifos.dev>
2022-10-29 21:15:03 +0000

    chore!: refactor to use Next.js This closes #4
    Signed-off-by: Rafael Julio <development@rafifos.dev>

---
+++ next.config.js
@@ -0,0 +1,53 @@
+/**
+ * Headers allow you to set custom HTTP headers for an incoming request path.
+ *
+ * @see [Headers configuration documentation](https://nextjs.org/docs/api-reference/next.config.js/headers)
+ */
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+
+  experimental: {
+    appDir: true,
+    fontLoaders: [{ loader: "@next/font/google", options: { subsets: ["latin"] } }],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import "./globalStyles.css";
+
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  display: "swap",
+  fallback: [
+    "Roboto",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
+  subsets: ["latin"],
+});
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR" className={inter.className}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <title>GainFit</title>
+      </head>
+
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
