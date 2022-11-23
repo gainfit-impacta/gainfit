216cc590b9a7578d70a0ed9eda167b06cca973fe
Rafael Julio <development@rafifos.dev>
2022-11-23 21:23:24 +0000

    feat: create sign-up form
    Signed-off-by: Rafael Julio <development@rafifos.dev>

---
+++ src/components/FormSignUp/index.tsx
@@ -0,0 +1,89 @@
+import type { User } from "interfaces";
+import type { SubmitHandler } from "react-hook-form";
+
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+interface FormSignUpProps {
+  onSubmit: SubmitHandler<User>;
+}
+
+function FormSignUp({ onSubmit }: FormSignUpProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<User>();
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">
+          Nome completo
+          <input
+            type="text"
+            id="name"
+            placeholder="João da Silva"
+            autoComplete="name"
+            {...register("name", {})}
+            {...(errors.name && { "aria-invalid": true })}
+          />
+          {errors.name && (
+            <span role="alert" className="input--error">
+              {errors.name.message}
+            </span>
+          )}
+        </label>
+
+        <label htmlFor="email">
+          E-mail
+          <input
+            type="email"
+            id="email"
+            placeholder="joão.silva@exemplo.com.br"
+            autoComplete="email"
+            {...register("email")}
+            {...(errors.email && { "aria-invalid": true })}
+          />
+          {errors.email && (
+            <span role="alert" className="input--error">
+              {errors.email.message}
+            </span>
+          )}
+        </label>
+
+        <label htmlFor="password">
+          Digite a sua senha
+          <input
+            type="password"
+            id="password"
+            placeholder="********"
+            autoComplete="current-password"
+            {...register("password", {
+              minLength: 8,
+              maxLength: 96,
+              pattern: /^[\w\W]+$/i,
+            })}
+            {...(errors.password && { "aria-invalid": true })}
+          />
+          {errors.password?.type === "pattern" && (
+            <span role="alert" className="input--error">
+              Precisa conter entre 8 e 96 caracteres, um número, uma letra maiúscula, uma letra
+              minúscula e um caractere especial
+            </span>
+          )}
+        </label>
+
+        <button type="submit" aria-busy={isSubmitting}>
+          Entrar
+        </button>
+      </form>
+
+      <small>
+        Já tem uma conta? <Link href="/auth/sign-in">Entre</Link>
+      </small>
+    </>
+  );
+}
+
+export default FormSignUp;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as CardGym } from "./CardGym";
 export { default as FormSignIn } from "./FormSignIn";
+export { default as FormSignUp } from "./FormSignUp";
